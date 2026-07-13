9944770a26fc507241ab53d450c9ec3b3968448e
Harshit Agrawala <harshit2z1agrawala@gmail.com>
2026-07-13 12:19:32 +0530

    Time: 0 ms (100%), Space: 12.3 MB (42.5%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> ans;
-        int l=0, r=0;
+        // using 3 pointers and no extra space.!
+        // use the extra space of nums1
+        // traverse from the back
 
-        // while loop-1 
-        while ( l<m && r<n ){
-            if ( nums1[l]<=nums2[r]){
-                ans.push_back(nums1[l]);
-                l++;
+        int i=m-1, j=n-1, k=m+n-1;
+
+        while ( i>=0 && j>=0){
+            if ( nums2[j]>nums1[i]){
+                nums1[k]=nums2[j];
+                k--;
+                j--;
             }
             else{
-                ans.push_back(nums2[r]);
-                r++;
+                nums1[k]=nums1[i];
+                k--;
+                i--;
             }
         }
-
-        // 2nd pass remaining elements
-        while ( r<n){
-            ans.push_back(nums2[r]);
-            r++;
-        }
-        while ( l<m){
-            ans.push_back(nums1[l]);
-            l++;
-        }
-
-        // ---------------replace elements
-        for ( int i = 0; i < m+n;  i++){
-            nums1[i]=ans[i];
+        // remaining elements of nums2
+        while ( j>=0){
+            nums1[k]=nums2[j];
+            k--;
+            j--;
         }
 
     }
