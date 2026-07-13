ff1715b20fb3d2a677a88d06a7480e22db817acf
Harshit Agrawala <harshit2z1agrawala@gmail.com>
2026-07-13 13:08:53 +0530

    Time: 0 ms (100%), Space: 15.1 MB (16.24%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -3,18 +3,16 @@ public:
     int diagonalSum(vector<vector<int>>& mat) {
         int sum=0;
         int size=mat.size();
+        // in pd: j=i
+        // in sd j=size-i-1
+
 
         for ( int i =0; i <size;i++ ){
-            
-            for ( int j=0;j<size; j++){
-                if ( i==j){
-                    sum+=mat[i][j];
-                }
-
-                else if (j== size-i-1){
-                    sum+=mat[i][j];
-                }
+            sum+=mat[i][i];
+            if(i!=size-i-1){
+                sum+=mat[i][size-i-1];
             }
+           
         }
 
         return sum;
