02097de09c60791160de6e1dab655b2ff9e889ee
Harshit Agrawala <harshit2z1agrawala@gmail.com>
2026-07-25 19:42:41 +0530

    Time: 0 ms (100%), Space: 8.7 MB (74.03%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        priority_queue<int> pq;
+        int max1=0;
+        int max2=0;
 
-        while(n!=0){
-            int digit = n%10;
-            pq.push(digit);
+        while ( n!=0){
+            int digit= n%10;
 
+            if ( digit>=max1 ){
+                max2 = max1;
+                max1= digit;
+            }
+            else if ( digit < max1 && digit> max2) {
+                max2 = digit;
+            }
             n=n/10;
         }
 
-        int a =  pq.top(); pq.pop();
-        int b= pq.top(); pq.pop();
-
-        return a*b;
+        return max1*max2;
     }
 };
