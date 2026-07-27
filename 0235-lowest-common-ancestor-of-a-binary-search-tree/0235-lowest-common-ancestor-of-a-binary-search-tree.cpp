c50bcd9c356e05f7ef3e0b69fe875b9c0107838f
Harshit Agrawala <harshit2z1agrawala@gmail.com>
2026-07-27 10:44:24 +0530

    Time: 27 ms (22.48%), Space: 23.5 MB (66.63%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,42 +10,22 @@
 
 class Solution {
 public:
-    void searchNode(TreeNode* node, int val, vector<TreeNode*>&path){
-        if(node==nullptr) return;
-
-        path.push_back(node);
-
-        if ( node->val==val ){
-            return ;
-        }
-        else if ( node->val > val){
-            if(node->left){
-                searchNode(node->left, val, path );
-            }
-        }
-        else {
-            if ( node->right ){
-                searchNode(node->right, val , path);
-            }
-        }
-    }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> path1;
-        searchNode(root, p->val, path1);
+        while ( root!=nullptr){
+            if(root==nullptr) return nullptr;
 
-        vector<TreeNode*> path2;
-        searchNode(root, q->val, path2);
+            if ( (p->val < root->val) && (q->val < root->val) ){
+                root=root->left;
+            }
+            else if ( (p->val > root->val) && (q->val > root->val) ){
+                root=root->right;
+            }
 
-        int n=path1.size();
-        int m=path2.size();
-        TreeNode* lca=0;
-        for ( int i =0; i< min(m, n); i++){
-            if ( path1[i]->val==path2[i]->val){
-                lca=path1[i];
+            else{
+                break;
             }
         }
-
-        return lca;
+        return root;
     }
 };
