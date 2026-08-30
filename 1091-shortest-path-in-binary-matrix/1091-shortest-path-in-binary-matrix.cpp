e4939c9a44bc85d2be186fd4bc802a2744d9f6da
Harshit Agrawala <harshit2z1agrawala@gmail.com>
2026-08-30 10:37:36 +0530

    Time: 263 ms (5.01%), Space: 83 MB (5.02%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,53 +1,52 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        typedef  pair<int, pair<int, int>> P;
+        
         if ( grid[0][0]!=0) return -1;
 
         int m=grid.size(); //nxn
 
-        vector<vector<int>> dir =  {{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1}};
+        vector<vector<int>> directions =  {{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1}};
+        vector< vector<int>> result(m, vector<int>(m, INT_MAX));
 
         // lambda
         auto isSafe = [&](int x, int y){
             return (x>=0 && x<m && y>=0 && y<m);
         };
 
-        // using BFS
-        queue<pair<int, int>> q;
+        priority_queue<P, vector<P>, greater<P> > pq;
 
-        q.push({0,0});
+        pq.push({0, {0,0}});
         grid[0][0]=1;
+        result[0][0]=0;
 
-        int count =0 ;
+        // use grid - to mark visited 
+        while (!pq.empty()){
+            int d = pq.top().first;
+            pair<int, int> node = pq.top().second;
 
-        while (!q.empty()){
-            // length of the queue , or current level 
-            int n = q.size();
+            int x = node.first;
+            int y = node.second;
 
-            // traverse all elements in queue
-            while ( n-- ){
-                int x = q.front().first;
-                int y = q.front().second;
-                q.pop();
+            pq.pop();
 
-                // base case, if target end reached
-                if (x==m-1 && y==m-1) return count+1;
+            for ( auto dir : directions){
+                int nr =  x+dir[0];
+                int nc = y +  dir[1];
 
-                for ( auto d: dir){
-                    int nr =  x + d[0];
-                    int nc =  y + d[1];
-
-                    if (isSafe(nr, nc) && grid[nr][nc]==0){
-                        q.push({nr, nc});
-
-                        grid[nr][nc]=1; //mark visited
-                    }
+                if ( isSafe(nr,nc) && grid[nr][nc]==0 && d + 1 < result[nr][nc]){
+                    pq.push({d+1, {nr, nc}});
+                    result[nr][nc]= d+1;
                 }
             }
 
-            count++;
         }
 
-        return -1;
+        if (result[m-1][m-1]==INT_MAX){
+            return -1;
+        }
+        
+        return result[m-1][m-1]+1;
     }
 };
